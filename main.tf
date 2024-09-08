2a408456ec74983a65c067aa4ccf58edf6882164
EC2 Default User <ec2-user@ip-172-31-26-190.ec2.internal>
2024-09-08 14:12:57 +0000

    vpc module test

---
+++ main.tf
@@ -0,0 +1,15 @@
+module "week4_vpc_test" {
+  source = "git::https://github.com/jagadeesh619/week4-vpc-module.git"  # Ensure to specify the source path of your module
+
+  vpc_cidr_block             = "10.0.0.0/16"
+  vpc_name                   = "week4_vpc"
+  vpc_igw_name               = "week4_igw"
+  public_subnet_cidr_block   = "10.0.1.0/24"
+  public_subnet_name         = "public_subnet"
+  private_subnet_cidr_block  = "10.0.11.0/24"
+  private_subnet_name        = "private_subnet"
+  ami_id                     = "ami-0182f373e66f89c85"
+  instance_type              = "t2.micro"
+  instance_name              = "week4_ec2"
+}
+
