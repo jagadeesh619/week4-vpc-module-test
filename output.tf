5b96677d4efba3031327709f6eb4bc3f24a81b13
jagadeesh619 <89119910+jagadeesh619@users.noreply.github.com>
2024-09-09 10:40:13 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "ec2_id" {
-  value = module.week4-vpc-module.ec2_id
+  value = module.week4_vpc_test.ec2_id
 }
 
 output "sg_id" {
-  value = module.week4-vpc-module.sg_id
+  value = module.week4_vpc_test.sg_id
 }
